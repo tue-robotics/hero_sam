7ccd168749e61a7157336a264159e668e351be4f
Utku Tahan <utku.tahan.ee@gmail.com>
2024-11-17 12:43:50 +0100

    refactor code

--- demo_fast_sam_algorithm.py
+++ demo_fast_sam_algorithm.py
@@ -3,32 +3,37 @@ import cv2
 from algorithms.fast_sam import fastSamRealTime
 
 
-model_size = "small"
-real_time_sam = fastSamRealTime(model_size=model_size)
+def main():
+    model_size = "small"
+    real_time_sam = fastSamRealTime(model_size=model_size)
 
-# Initiate camera -> 0:embedded camera of laptop, 1,2,3...: external cam devs
-cap = cv2.VideoCapture(0)
+    # Initiate camera -> 0:embedded camera of laptop, 1,2,3...: external cams
+    cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
+    while cap.isOpened():
 
-    suc, frame = cap.read()
+        suc, frame = cap.read()
 
-    everything_results = real_time_sam.model(
-        source=frame,
-        device=real_time_sam.device,
-        retina_masks=True,
-        imgsz=1024,
-        conf=0.4,
-        iou=0.9,
-    )
+        everything_results = real_time_sam.model(
+            source=frame,
+            device=real_time_sam.device,
+            retina_masks=True,
+            imgsz=1024,
+            conf=0.4,
+            iou=0.9,
+        )
 
-    img = real_time_sam.wrapper_fastsam_prompt(frame, everything_results)
+        img = real_time_sam.wrapper_fastsam_prompt(frame, everything_results)
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('img', img)
+        cv2.imshow('frame', frame)
+        cv2.imshow('img', img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cv2.destroyAllWindows()
-cap.release()
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+if __name__ == "__main__":
+    main()
