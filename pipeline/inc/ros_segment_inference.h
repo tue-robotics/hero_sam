b6f90a73ca8d28e0aa9e4b96c948f41834523d5c
IasonTheodorou <iasonth95@gmail.com>
2025-05-10 19:36:08 +0200

    rso segmentation functionality to be able to run as a function called in a node

---
+++ pipeline/inc/ros_segment_inference.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "yolo_inference.h"
+#include "sam_inference.h"
+#include <iostream>
+#include <iomanip>
+#include <filesystem>
+#include <fstream>
+#include <random>
+//#include <ros/ros.h>
+
+
+// Forward declarations
+class YOLO_V8;
+
+// Declare the functions from main.cpp
+void Detector(YOLO_V8*& p, std::vector<DL_RESULT>& res, const cv::Mat& img);
+void Classifier(YOLO_V8*& p, const cv::Mat& img);
+int ReadCocoYaml(YOLO_V8*& p);
+//void DetectTest(cv::Mat& img);
+void DetectTest(const cv::Mat& img);
+void ClsTest();
+void ClassificationInference(const cv::Mat& img);
